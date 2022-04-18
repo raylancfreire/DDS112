918b5ed809c4eb9b8bda63e234240a088ca89351
Felype Invictus <felype.sales@outlook.com>
2022-04-18 17:21:59 -0300

    Raylan - DDS 1-12

--- src/ContaBancária/exercicios_professor_filipe/Exercicio09.java
+++ src/ContaBancária/exercicios_professor_filipe/Exercicio09.java
@@ -1,16 +1,12 @@
 package ContaBancária.exercicios_professor_filipe;
 
-import java.util.Scanner;
-
-public class Exercicio07 {
+public class Exercicio09 {
     public static void main(String[] args) {
         Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
         for(int i = 0; i < numeros.length; i++) {
-            Integer produto = numeros[i];
-            System.out.println("Produto de código: " + produto);
-            if (produto.equals(225)) {
-                System.out.println("Produto encontrado!");
-                break;
+            Integer numeroPercorrido = numeros[i];
+            if (numeroPercorrido % 3 == 0) {
+                System.out.println("Número divisível por 3: " + numeroPercorrido);
             }
         }
     }
